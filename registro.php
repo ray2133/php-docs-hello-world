3c1328df24f2bd21e2ece19c430affeb4c6c78d6
ray2133 <114694048+ray2133@users.noreply.github.com>
2022-12-01 10:26:40 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -2,22 +2,21 @@
 error_reporting(E_ALL);
 ini_set('display_errors',E_ALL);
 
-$db_database=getenv('information_schema');
-$db_password=getenv('daxter12/');
-$db_usarname=getenv('webappplication');
-$mysql_attr_ssl_ca=getenv('DigiCertGlobalRootCA.crt (1).pem');
+$db_database=getenv('tabla');
+$db_password=getenv('ramses-1234');
+$db_usarname=getenv('ray');
+$mysql_attr_ssl_ca=getenv('DigiCertGlobalRootCA.crt.pem');
 
 $conn = mysqli_init(); 
 if (!$conn) {
     die('Falló mysqli_init');
 }
-mysqli_ssl_set($conn,NULL,NULL, $DigiCertGlobalRootCA, NULL, NULL); 
+mysqli_ssl_set($conn,NULL,NULL, $mysql_attr_ssl_ca, NULL, NULL); 
 
-mysqli_real_connect($conn, "lmatephpdb2-server.mysql.database.azure.com", $webapplication, $daxter12, $information_schema, 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "lmatephpdb2-server.mysql.database.azure.com", $db_username, $db_password , $db_database, 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 echo 'Éxito... ' . $conn->host_info . "\n";
-?>
