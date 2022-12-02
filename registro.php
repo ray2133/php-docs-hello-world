770ee261a1d5872ef58588cb69a163aa2eb2296a
ray2133 <114694048+ray2133@users.noreply.github.com>
2022-12-02 12:29:24 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,4 +1,4 @@
 <?php
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "./DigiCertGlobalRootCA.crt", NULL, NULL);
 mysqli_real_connect($conn, "basededatos2.mysql.database.azure.com", "ramses", "hernandez-19", "hernandez", 3306, MYSQLI_CLIENT_SSL);
